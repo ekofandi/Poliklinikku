3b5a492f39bbaeba9c7360488608cbcff0d7f09f
ekofandi <73423344+ekofandi@users.noreply.github.com>
2023-10-24 03:01:54 +0700

    update db

--- poliklinik.sql
+++ poliklinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2023 at 03:57 AM
+-- Generation Time: Oct 23, 2023 at 09:57 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -34,6 +34,15 @@ CREATE TABLE `dokter` (
   `no_hp` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama`, `alamat`, `no_hp`) VALUES
+(2, 'Dr. Santoso', 'Jl. Satria Barat', '0821321234678'),
+(3, 'Dr. Anwar Santoso', 'Jl. Teratai No.11 Semarang', '081231237581'),
+(4, 'Dr. Chelsea Yunite', 'Dr. Madurokah No.21 Semarang', '0877123578312');
+
 -- --------------------------------------------------------
 
 --
@@ -41,11 +50,20 @@ CREATE TABLE `dokter` (
 --
 
 CREATE TABLE `pasien` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) NOT NULL,
   `nama` varchar(255) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
   `no_hp` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `nama`, `alamat`, `no_hp`) VALUES
+(1, 'Anggito ', 'Jl. Imam Bonjol', '082312345678'),
+(2, 'Imang', 'Jl. Berkah Jaya No.123 Semarang', '089123458122'),
+(3, 'Azizah', 'Jl. Brotojoye', '08765432123');
 
 -- --------------------------------------------------------
 
@@ -60,6 +78,29 @@ CREATE TABLE `periksa` (
   `tgl_periksa` datetime NOT NULL,
   `catatan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `username`, `password`, `level`) VALUES
+(1, 'Paijo', 'paijo', 'qwerty123', 'admin'),
+(2, 'Qulbi', 'qulbi', 'qulbi123', 'pasien'),
+(3, 'Yuni', 'yuni', 'yuni234', 'perawat');
 
 --
 -- Indexes for dumped tables
@@ -84,6 +125,12 @@ ALTER TABLE `periksa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -91,19 +138,25 @@ ALTER TABLE `periksa`
 -- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `periksa`
 --
 ALTER TABLE `periksa`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
